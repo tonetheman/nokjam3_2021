54a571e72ec15f00f55d3d95dd776b84c0e0a41e
tone <tonetheman@gmail.com>
2021-02-02 20:26:28 -0600

    stuff moving

--- main.js
+++ main.js
@@ -15,14 +15,32 @@ class Scene extends Phaser.Scene {
         g.destroy();
     }
     
+    preload() {
+        this.load.image("face", "nokia_face.png");
+        this.load.image("plat1", "nokia_platform1.png");
+        this.load.image("bg", "nokia_bg.png");
+    }
+
+    jump() {
+
+    }
+
     create() {
         this.createTextures();
 
-        sprites.push(this.add.sprite(0,16,"t"));
-        sprites.push(this.add.sprite(0,32,"t"));
+        this.bg = this.add.tileSprite(84/2, 48/2,
+            48,48, "bg");
+
+            this.add.sprite(20,20,"face");
+
+        //sprites.push(this.add.sprite(16,16,"t"));
+        //sprites.push(this.add.sprite(0,32,"t"));
+        //sprites.push(this.add.sprite(0,0,"face"))
     }
 
     update() {
+        this.bg.tilePositionX += 1;
+        /*
         sprites[0].x += 1;
         if (sprites[0].x>84) {
             sprites[0].x = 0;
@@ -32,6 +50,7 @@ class Scene extends Phaser.Scene {
         if (sprites[1].x>84) {
             sprites[1].x = 0;
         }
+        */
     }
 }
 
@@ -40,7 +59,7 @@ let config = {
     width : 84,
     height : 48,
     pixelArt : true,
-    zoom : 4,
+    zoom : 8,
     backgroundColor : "#c7f0d8",
     scene : [Scene]
 }
