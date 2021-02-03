cc72a7e52792ebe269dc39c38a7cd7cec077b7e2
tone <tonetheman@gmail.com>
2021-02-02 19:32:22 -0600

    paralax

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 
-let s = null;
+let sprites = [];
 
 class Scene extends Phaser.Scene {
     constructor() {
         super("game")
     }
+    
     createTextures() {
         let g = this.add.graphics();
         g.fillStyle(0x43523d);
@@ -13,16 +14,23 @@ class Scene extends Phaser.Scene {
         let tx = g.generateTexture("t",2,2);
         g.destroy();
     }
+    
     create() {
-        console.log("create");
         this.createTextures();
 
-        s = this.add.sprite(0,16,"t")
+        sprites.push(this.add.sprite(0,16,"t"));
+        sprites.push(this.add.sprite(0,32,"t"));
     }
+
     update() {
-        s.x += 1;
-        if (s.x>84) {
-            s.x = 0;
+        sprites[0].x += 1;
+        if (sprites[0].x>84) {
+            sprites[0].x = 0;
+        }
+
+        sprites[1].x += 2;
+        if (sprites[1].x>84) {
+            sprites[1].x = 0;
         }
     }
 }
